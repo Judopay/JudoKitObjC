2e5da65aeacaff5561703dd418c0a6a7b2a27c68
Mihai <mihai.petrenco@endava.com>
2019-12-05 21:59:33 +0200

    Added custom transition

---
+++ Source/Presentations/SliderPresentationController.h
@@ -0,0 +1,17 @@
+//
+//  SliderPresentationController.h
+//  TransitionDemo
+//
+//  Created by Mihai Petrenco on 12/5/19.
+//  Copyright © 2019 Mihai Petrenco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SliderPresentationController : UIPresentationController
+
+@end
+
+NS_ASSUME_NONNULL_END
